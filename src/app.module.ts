fb816a89dfe74d5cac9be0d090ae9574f9dbb8b2
goombeer <highbridge71@gmail.com>
2024-09-08 10:25:45 +0900

    POSTリクエストの実装

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,39 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import config from '../config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AppController } from './app.controller';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { LoggerMiddleware } from './middleware/logger';
 
 @Module({
-  imports: [],
+  imports: [
+    AppModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('database.host'),
+        port: configService.get('database.port'),
+        username: configService.get('database.username'),
+        password: configService.get('database.password'),
+        database: configService.get('database.name'),
+        entities: ['dist/**/entities/**/*.entity.js'],
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(LoggerMiddleware).forRoutes('');
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  async create({ name }: CreateUserDto): Promise<User> {
+    return await this.userRepository
+      .save({
+        name: name,
+      })
+      .catch((e) => {
+        throw new InternalServerErrorException(
+          `[${e.message}]：ユーザーの登録に失敗しました。`,
+        );
+      });
+  }
+}
